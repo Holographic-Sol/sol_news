0ee15dc34f77be1ad0918e903952c4a3de2cc186
Sol <36607894+Holographic-Sol@users.noreply.github.com>
2019-12-08 11:53:52 +0000

    Add files via upload

--- news_collector_android.py
+++ news_collector_android.py
@@ -1,7 +1,20 @@
 import subprocess
 import distutils.dir_util
+import psutil
 
-dat_dir = './news_articles/'
+dat_file_0 = './data.dat'
+if not os.path.exists(dat_file_0):
+    open(dat_file_0, 'w').close()
+
+flag = False
+xcmd_process = []
+
+# Subprocess Info
+info = subprocess.STARTUPINFO()
+info.dwFlags = 1
+info.wShowWindow = 0
+
+dat_dir = './news_articles'
 distutils.dir_util.mkpath(dat_dir)
 
 crawler = ['somersetlive_somerset-news.py',
@@ -12,7 +25,7 @@ crawler = ['somersetlive_somerset-news.py',
            'express_news-science.py',
            'express_news-weather.py',
            'express_science-technology.py',
-           'techradar_uk-pro.py'
+           'techradar_uk-pro.py',
            'express_entertainment-gaming.py',
            'express_news-nature.py',
            'express_news-weird.py',
@@ -23,9 +36,210 @@ crawler = ['somersetlive_somerset-news.py',
            'metro_news-world.py',
            'express_entertainment-films.py']
 
-i = 0
-for crawlers in crawler:
-    cmd = 'python '+crawler[i]
+
+def funk1():
+    var = crawler[0]
+    cmd = 'python ' + var
     print('running command:', cmd)
     xcmd = subprocess.Popen(cmd, shell=True)
-    i += 1
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk2():
+    var = crawler[1]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk3():
+    var = crawler[2]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+def funk4():
+    var = crawler[3]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk5():
+    var = crawler[4]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk6():
+    var = crawler[5]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk7():
+    var = crawler[6]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk8():
+    var = crawler[7]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk9():
+    var = crawler[8]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+def funk10():
+    var = crawler[9]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk11():
+    var = crawler[10]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk12():
+    var = crawler[11]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk13():
+    var = crawler[12]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk14():
+    var = crawler[13]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk15():
+    var = crawler[14]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk16():
+    var = crawler[15]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk17():
+    var = crawler[16]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+def funk18():
+    var = crawler[17]
+    cmd = 'python ' + var
+    print('running command:', cmd)
+    xcmd = subprocess.Popen(cmd, shell=True)
+    xcmd_pid = xcmd.pid
+    xcmd_process.append(xcmd_pid)
+    print('subprocess PID :', xcmd_pid)
+
+
+# funk1()
+# funk2()
+# funk3()
+# funk4()
+# funk5()
+# funk6()
+# funk7()
+# funk8()
+# funk9()
+# funk10()
+# funk11()
+# funk12()
+funk13()
+# funk14()
+# funk15()
+# funk16()
+# funk17()
+# funk18()
+
+while flag is False:
+    i = 0
+    for xcmd_processs in xcmd_process:
+    #     if psutil.pid_exists(xcmd_process[i]) is True:
+    #         pass
+    #     else:
+    #         del xcmd_process[i]
+    #         if len(xcmd_process) is 0:
+    #             flag = True
+        i += 1
